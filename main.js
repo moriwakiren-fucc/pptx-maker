0ff0a5339cf44fbca57cd213e1e8ed53651c791e
moriwakiren-fucc <moriwakiren.fucc@gmail.com>
2026-02-25 11:49:25 +0900

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ function copyButton(elementId) {
     // 上記要素をクリップボードにコピーする
     navigator.clipboard.writeText(element.textContent)
 }
+
 const runBtn = document.getElementById("runBtn");
 const codeInput = document.getElementById("codeInput");
 const errorBox = document.getElementById("errorBox");
