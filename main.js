9b608ca5635fc876386482f8cf7a344b13e1fe30
moriwakiren-fucc <moriwakiren.fucc@gmail.com>
2026-02-26 10:55:44 +0900

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function copyButton() {
 
   // input / textarea を除去（文字化け防止）
   baseText.querySelectorAll("input, textarea").forEach(el => el.remove());
-  let result = baseText.textContent.trim().replace("- スライドの枚数は枚以上枚以下とする", `- スライドの枚数は ${min} 枚以上 ${max} 枚以下とする`).replace("\n以下の欄にその他の条件を記入することもできます。", demand) + content;
+  let result = `${baseText.textContent.trim().replace("- スライドの枚数は枚以上枚以下とする", `- スライドの枚数は ${min} 枚以上 ${max} 枚以下とする`).replace("\n以下の欄にその他の条件を記入することもできます。", demand)}${content};
   navigator.clipboard.writeText(result);
   const btn = document.getElementById('copybutton');	
   btn.textContent = 'コピー完了';
